5c8f80372632a519e5edbb9ccebc0f60f20ff8d2
hanyue1014 <47289292+hanyue1014@users.noreply.github.com>
2024-03-21 13:39:14 +0800

    Add Round Button and "Bottom Sheet"

--- app/src/main/java/com/example/abcdefg/GroupActivity.kt
+++ app/src/main/java/com/example/abcdefg/GroupActivity.kt
@@ -1,11 +1,24 @@
 package com.example.abcdefg
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.abcdefg.databinding.ActivityGroupBinding
 
 class GroupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGroupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_group)
+        binding = ActivityGroupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnExpandBtm.setOnClickListener {
+            binding.btmExpand.visibility = if (binding.btmExpand.visibility == View.GONE) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
+        }
     }
 }
