fe16626b5d953346888d415d835076394d62093b
hanyue1014 <47289292+hanyue1014@users.noreply.github.com>
2024-03-25 18:21:37 +0800

    Add blogs section to user profile

--- app/src/main/java/com/example/abcdefg/UserProfileActivity.kt
+++ app/src/main/java/com/example/abcdefg/UserProfileActivity.kt
@@ -3,11 +3,39 @@ package com.example.abcdefg
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.abcdefg.databinding.ActivityGroupMainBinding
+import com.example.abcdefg.databinding.ActivityUserProfileBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
+val BlogTab = arrayOf(
+    "My Blogs",
+    "Saved Blogs"
+)
+class ViewPagerAdapter(manager: FragmentManager, lifecycle: Lifecycle): FragmentStateAdapter(manager, lifecycle) {
+    override fun getItemCount(): Int {
+        return 2 // always got two tabs only
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> MyBlogsFragment()
+            1 -> SavedBlogsFragment()
+            else -> MyBlogsFragment() // wont trigger but needed
+        }
+    }
+
+}
 
 class UserProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUserProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityUserProfileBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_profile)
+        setContentView(binding.root)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
             overrideActivityTransition(OVERRIDE_TRANSITION_OPEN, R.anim.slide_in_from_bottom, 0)
@@ -15,6 +43,13 @@ class UserProfileActivity : AppCompatActivity() {
         } else {
             overridePendingTransition(R.anim.slide_in_from_bottom, 0)
         }
+
+        // bind view pager adapter
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        binding.vp2User.adapter = viewPagerAdapter
+        TabLayoutMediator(binding.tlBlogs, binding.vp2User) { tab, position ->
+            tab.text = BlogTab[position]
+        }.attach()
     }
 
     override fun onPause() {
