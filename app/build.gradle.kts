c5b8b935343e9799eff71a5d5396c99ba59b8ec1
hanyue1014 <47289292+hanyue1014@users.noreply.github.com>
2024-04-29 16:12:30 +0800

    Link with firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,9 @@ android {
 
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
--- app/src/main/java/com/example/abcdefg/MainActivity.kt
+++ app/src/main/java/com/example/abcdefg/MainActivity.kt
@@ -5,17 +5,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.abcdefg.databinding.ActivityGroupMainBinding
 import com.example.abcdefg.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+
         binding.login.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
+
+        binding.signUp.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // if alrd logged in then can proceed lo
+        if (auth.currentUser != null) {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+
     }
 }
