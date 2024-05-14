50c69772cfc5d80be831ed2a6d7db84e77f2f97c
hanyue1014 <47289292+hanyue1014@users.noreply.github.com>
2024-05-14 11:59:07 +0800

    Add dependency

--- app/src/main/java/com/example/abcdefg/data/Event.kt
+++ app/src/main/java/com/example/abcdefg/data/Event.kt
@@ -3,4 +3,12 @@ package com.example.abcdefg.data
 import java.util.Date
 
 // TODO Review this
-data class Event(val name: String, val description: String, val eventDate: Date, val joinedBy: Array<User>)
+data class Event(
+    val name: String  = "",
+    val description: String = "",
+    val eventDate: Date,
+    val eventStartTime: Date,
+    val eventEndTime: Date,
+    val imgPath: String,
+    val joinedBy: List<String>,
+)
